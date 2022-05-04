6598c59aee210c3578a3c915928953ce8354650e
śhanhaider23 <shan.haider@gmail.com>
2022-05-04 17:37:16 +0200

    deleted files backend

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const config = require('./utils/config')
-const http = require('http')
-
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
-    console.log(`Server running on PORT ${config.PORT}`)
-})
--- backend/utils/middleware.js
+++
@@ -1,9 +0,0 @@
-const tokenExtractor = (request, response, next) => {
-    const authorization = request.get('Authorization')
-    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        request.token = authorization.substring(7)
-    }
-    next()
-}
-
-module.exports = { tokenExtractor }
